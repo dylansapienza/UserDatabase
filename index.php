2f5cf7924353173c2bad92440bc97d7779a6fec3
Dylan Sapienza <dylans@gwupyterhub.seas.gwu.edu>
2020-02-15 13:27:41 -0500

    Added Index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Dylan's App</title>
+    </head>
+
+    <body>
+        <h1>Welcome to my App!</h1></b>
+        <h3>Please Login!</h3></b>
+            <p>If you haven't made an account, register!</p>
+            <a href="login.php">
+                <img src="login.png" alt="Login" style="width:140px;height:50px;border:0">
+            </a>
+            <a href="register.php">
+                <img src="register.png" alt="Register" style="width:140px;height:50px;border:0">
+            </a>
+
+
+
+    </body>
+
+</html>
